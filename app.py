10a85a1c1a2e53b7543ad5153d07ba21047b13bf
Wilbert <wilbertliu1999@gmail.com>
2021-11-03 20:57:03 +0000

    Added app.run function and requirements.txt

---
+++ app.py
@@ -0,0 +1,40 @@
+# Flask imports
+import flask
+from flask import Flask
+from flask import render_template
+
+# Database imports
+from flask_sqlalchemy import SQLAlchemy
+
+# API tools
+import os
+
+# Flask app initialization and db setup
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# App Routing
+@app.route("/")
+def index():
+    db.create_all()
+    return flask.redirect(flask.url_for("login_page"))
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+    
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/user")
+def user_page():
+    return render_template("user.html")
+
+
+
+app.run(
+    debug=True
+    # host="0.0.0.0",
+    # port=int(os.getenv("PORT", 8080)),
+)
