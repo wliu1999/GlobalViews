8effd020615f934d99a7940500a2db9eee62f9f6
Ubuntu <wliu19@i-hate-vms.doo322vjvnzulnsysway5jbksd.bx.internal.cloudapp.net>
2021-11-17 04:25:25 +0000

    Trimmed requirements and enabled production mode

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def save_favorite():
 # For testing, comment out host and port lines.
 
 app.run(
-    debug=True
-    # host="0.0.0.0",
-    # port=int(os.getenv("PORT", 8080)),
+    # debug=True
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", 8080)),
 )
