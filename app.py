baea2c2524eabd9407853eeea3d430acae3f892d
Adarsh830 <aaa830@gmail.com>
2021-11-30 20:04:53 +0000

    requirements update

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ scopes = [
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=scopes,
-    redirect_uri="http://127.0.0.1:5000/callback",
+    redirect_uri="https://global-views.herokuapp.com/callback",
 )
 
 login_manager = LoginManager(app)
@@ -233,7 +233,7 @@ def save_favorite():
 # For testing, comment out host and port lines.
 
 app.run(
-    debug=True
-    # host="0.0.0.0",
-    # port=int(os.getenv("PORT", 8080)),
+    # debug=True
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", 8080)),
 )
